cd0aaed14fe607deb76989a48b4a73059c57bd2d
Ray Johnson <rayj@lindenlab.com>
2024-07-31 11:31:46 -0700

    more on completions

--- update_completions.sh
+++ update_completions.sh
@@ -2,3 +2,7 @@
 # Updates bash completions from select commands that generate them
 
 chezmoi completion bash > dot_local/share/bash-completion/completions/chezmoi
+
+docker completion bash > dot_local/share/bash-completion/completions/docker
+
+gh completion -s bash > dot_local/share/bash-completion/completions/gh
